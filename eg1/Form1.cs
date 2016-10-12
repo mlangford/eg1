726ebb1f388505dcc9e98133c557a23bd6698cb4
Mitch <mitchel.langford@southwales.ac.uk>
2016-10-12 15:45:18 +0100

    Stage1
    adds two numbers

--- eg1/Form1.cs
+++ eg1/Form1.cs
@@ -16,5 +16,37 @@ namespace eg1
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            btnAdd.Enabled = (textBox1.Text.Length > 0 && textBox2.Text.Length > 0);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            btnAdd.Enabled = (textBox1.Text.Length > 0 && textBox2.Text.Length > 0);
+        }
+
+        float c;
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            float a;
+            float b;
+            a = Convert.ToSingle(textBox1.Text);
+            b = Convert.ToSingle(textBox2.Text);
+
+            c = (a + b) * 1.2F;
+
+            textBox3.Text = c.ToString("C");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(c.ToString());
+        }
     }
 }
